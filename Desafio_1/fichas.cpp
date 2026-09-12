d0112ed3760b8bc9471a689b8aed6421fd8379f7
Dani Ayala <117943720+DaniAyala3103@users.noreply.github.com>
2026-09-12 16:41:59 -0500

    se declararon las funciones leerFicha y escribirFicha en el modulo de fichas

--- Desafio_1/fichas.cpp
+++ Desafio_1/fichas.cpp
@@ -1,3 +1,15 @@
 #include "fichas.h"
 
-fichas::fichas() {}
+int calcularByte(int fila, int columna, int columnas)
+{
+    int indice= fila*columnas+columna;
+    int bitInicial= indice *3;
+    return bitInicial /8;
+}
+
+int posicionEnByte(int fila, int columna, int columnas)
+{
+    int indice = fila*columnas+columna;
+    int bitInicial = indice*3;
+    return bitInicial %8;
+}
--- Desafio_1/fichas.h
+++ Desafio_1/fichas.h
@@ -1,10 +1,10 @@
 #ifndef FICHAS_H
 #define FICHAS_H
 
-class fichas
-{
-public:
-    fichas();
-};
+int calcularByte(int fila, int columna, int columnas);
+int posicionEnByte(int fila, int columna, int columnas);
+
+unsigned char leerFicha(unsigned char* memoria, int fila, int columna, int columnas);
+void escribirFicha(unsigned char* memoria, int fila, int columna, int columnas, unsigned char valor);
 
 #endif // FICHAS_H
