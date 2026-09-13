4c14cd67f46faa42b27675e175cd4ae0106f51f8
Dani Ayala <117943720+DaniAyala3103@users.noreply.github.com>
2026-09-13 18:56:20 -0500

    implementamos y validamos leerficha y escribir fichas usando el debugger para ir tanteando que todo vaya bien, xd

--- Desafio_1/fichas.cpp
+++ Desafio_1/fichas.cpp
@@ -13,3 +13,64 @@ int posicionEnByte(int fila, int columna, int columnas)
     int bitInicial = indice*3;
     return bitInicial %8;
 }
+
+unsigned char leerFicha(unsigned char *memoria, int fila, int columna, int columnas)
+{
+    int byte= calcularByte(fila,columna,columnas);
+    int pos = posicionEnByte(fila,columna,columnas);
+
+    unsigned char valor;
+    if(pos <= 5)
+    {
+        //caso ideal donde la ficha cabe perfectamente en un solo byte
+        valor=(memoria[byte]>>(5-pos))&7;
+
+    }
+    else if (pos==6)
+    {
+        //caso con 2 bits en memoria[byte], 1 bit en memoria[byte+1]
+        unsigned char parteAlta=memoria[byte]&3;        //los 2 bits menos significativos de este byte
+        unsigned char parteBaja=(memoria[byte+1]>>7)&1; //el bit mas significativo del siguiente byte
+        valor = (parteAlta<<1)|parteBaja;
+    }
+    else //pos ==7
+    {
+        //caso con 1 bit en memoria[byte], 2 bits en memoria[byte+1]
+        unsigned char parteAlta=memoria[byte]&1;            //el bit menos significativo de este byte
+        unsigned char parteBaja=(memoria[byte+1]>>6)&3;     //los 2 bits mas significativos del siguiente byte
+        valor = (parteAlta<<2)| parteBaja;
+    }
+
+    return valor;
+}
+
+void escribirFicha(unsigned char *memoria, int fila, int columna, int columnas, unsigned char valor)
+{
+    int byte = calcularByte(fila, columna, columnas);
+    int pos= posicionEnByte(fila, columna,columnas);
+
+    if(pos<=5)
+    {
+        //caso ideal en el que la ficha cabe perfectamente en un solo byte
+        unsigned char mascara = ~(7<<(5-pos));
+        memoria[byte]=(memoria[byte]&mascara)|(valor<<(5-pos));
+
+    }
+    else if (pos ==6)
+    {
+        //caso con 2 bits en memoria[byte], 1 bit en memoria[byte+1]
+        unsigned char parteAlta= (valor>>1)&3;  //los 2 bits mas significativos del valor.
+        unsigned char parteBaja= valor&1;   //el bit menos significativo del valor
+        memoria[byte] = memoria[byte]&~3 | parteAlta;
+        memoria[byte+1]= (memoria[byte+1]& ~(1<<7)) | (parteBaja<<7);
+
+    }
+    else //pos ==7
+    {
+        //caso 1 bit en moemoria[byte] y 2 en memoria[byte+1]
+        unsigned char parteAlta =(valor>>2)&1;        //el bit mas significativo
+        unsigned char parteBaja = valor & 3;                    //los 2 bits menos significativos
+        memoria[byte]=(memoria[byte]& ~1)|parteAlta;
+        memoria[byte+1]=(memoria[byte+1]& ~(3<<6))|(parteBaja<<6);
+    }
+}
--- Desafio_1/main.cpp
+++ Desafio_1/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-
+#include "memoria.h"
+#include "fichas.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    // int filas = 3, columnas = 3;
+    // unsigned char* memoria = reservarMemoria(filas, columnas);
+
+    // // Prueba caso simple
+    // escribirFicha(memoria, 0, 0, columnas, 5);
+    // unsigned char r1 = leerFicha(memoria, 0, 0, columnas); // debería ser 5
+
+    // // Prueba con varias fichas para forzar casos repartidos entre bytes
+    // for (int f = 0; f < filas; f++)
+    //     for (int c = 0; c < columnas; c++)
+    //         escribirFicha(memoria, f, c, columnas, (f+c) % 6); // valores 0-5
+
+    // bool ok = true;
+    // for (int f = 0; f < filas; f++)
+    //     for (int c = 0; c < columnas; c++)
+    //         if (leerFicha(memoria, f, c, columnas) != (f+c) % 6)
+    //             ok = false;
+
+
+    // delete[] memoria;
+    // return 0;
 }
