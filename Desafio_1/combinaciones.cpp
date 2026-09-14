223aeaa723ea060ea6ae6a14a8f5c994c32ee120
Dani Ayala <117943720+DaniAyala3103@users.noreply.github.com>
2026-09-14 02:56:29 -0500

    se implementa la deteccion de combinaciones horizontales y vericales usando el arreglo de marcas

--- Desafio_1/combinaciones.cpp
+++ Desafio_1/combinaciones.cpp
@@ -1,3 +1,72 @@
 #include "combinaciones.h"
+#include "fichas.h"
 
-combinaciones::combinaciones() {}
+bool *crearMarcas(int filas, int columnas)
+{
+    bool *marcas =new bool[filas *columnas];
+    for(int i=0; i<filas*columnas; i++)
+        marcas[i]=false;
+    return marcas;
+}
+void liberarMarcas(bool *marcas)
+{
+    delete[]marcas;
+}
+
+void marcasHorizontales(unsigned char *memoria, int filas, int columnas, bool *marcas)
+{
+    for(int f=0; f < filas; f++)
+    {
+        int c=0;
+        while (c<columnas)
+        {
+            unsigned char valor =leerFicha(memoria, f, c, columnas);
+            int inicio = c;
+
+            while(c< columnas && leerFicha(memoria,f,c,columnas)==valor)
+                c++;
+
+            int largo =c-inicio;
+
+            if(valor<= 5&& largo>=3)// solo fichas validas, no vacio ni especial.
+            {
+                for (int k=inicio; k<c; k++)
+                    marcas[f*columnas+k]=true;
+
+            }
+        }
+    }
+}
+
+bool hayCombinaciones(bool *marcas, int filas, int columnas)
+{
+    for(int i=0; i<filas*columnas; i++)
+        if(marcas[i])
+            return true;
+    return false;
+}
+
+void marcasVerticales(unsigned char *memoria, int filas, int columnas, bool *marcas)
+{
+    for(int c=0; c<columnas; c++)
+    {
+        int f=0;
+        while(f<filas)
+        {
+            unsigned char valor =leerFicha(memoria, f,c,columnas);
+            int inicio = f;
+
+            while(f<filas && leerFicha(memoria,f,c,columnas)==valor)
+                f++;
+
+            int largo = f-inicio;
+
+            if(valor<= 5&& largo>=3)        //solo reciba fichas validas, nada de vacio o especiales.
+            {
+                for(int k=inicio; k<f; k++)
+                    marcas[k*columnas+c]=true;
+            }
+        }
+    }
+}
+
--- Desafio_1/combinaciones.h
+++ Desafio_1/combinaciones.h
@@ -1,10 +1,10 @@
 #ifndef COMBINACIONES_H
 #define COMBINACIONES_H
 
-class combinaciones
-{
-public:
-    combinaciones();
-};
+bool *crearMarcas(int filas, int columnas);
+void liberarMarcas(bool *marcas);
+void marcasHorizontales(unsigned char* memoria, int filas, int columnas, bool *marcas);
+void marcasVerticales(unsigned char* memoria, int filas, int columnas, bool* marcas);
+bool hayCombinaciones(bool* marcas, int filas, int columnas);
 
 #endif // COMBINACIONES_H
